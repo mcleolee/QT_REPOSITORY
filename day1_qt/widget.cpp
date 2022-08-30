1ab04fb2154f780361b649dbfcd3e01c34f8625a
Raven <1772133019@qq.com>
2022-08-30 13:48:49 +0800

    1348

--- day1_qt/widget.cpp
+++ day1_qt/widget.cpp
@@ -14,8 +14,10 @@ Widget::Widget(QWidget *parent)
     // 将当前界面类对象 this 指针与 ui 指针关联
     // 这样 ui 指针才能访问操作 GUI 界面的控件
     ui->setupUi(this);
-    qDebug() << "hello world\n";
-    qDebug("hello world haha\n");
+    qDebug() << "hello world";
+    qDebug("hello world haha");
+    qDebug();
+    qDebug() << "123" << "hello world lmao" << 123;
 }
 
 Widget::~Widget()
