0bbcab119d4b631a9cb63eeb08385d8097b5ad25
Raven <1772133019@qq.com>
2022-08-30 14:45:35 +0800

    1445

--- day1_qt/widget.cpp
+++ day1_qt/widget.cpp
@@ -14,10 +14,24 @@ Widget::Widget(QWidget *parent)
     // 将当前界面类对象 this 指针与 ui 指针关联
     // 这样 ui 指针才能访问操作 GUI 界面的控件
     ui->setupUi(this);
+
+    // qt 中所有类库提供的类名或头文件，都是大写字母开头
+    // qt 中所有接口函数，都是小写字母开头
+
+    // qDebug(): 函数内部构造类一个默认的输出流对象
     qDebug() << "hello world";
     qDebug("hello world haha");
     qDebug();
     qDebug() << "123" << "hello world lmao" << 123;
+    qDebug("123");
+    QString str = QString("123") + QString("qwe");
+    qDebug() << str;
+    // qDebug() 格式化输出
+    qDebug("%d",69);
+
+    int a=69, b=96;
+    qDebug() << a+b;
+    qDebug("%d + %d = %d",a,b,a+b);
 }
 
 Widget::~Widget()
